37cd488bb3c590e77f5fb097324323f5d7b303ae
Jheng-Yi Chen <deanst28@gmail.com>
2018-03-03 22:54:33 +0800

    Update remove first "," and last ",.R

--- remove first "," and last ",.R
+++ remove first "," and last ",.R
@@ -1,3 +1,2 @@
-
 Data$variable1[str_sub(string = Data$variable1, start = 1, end = 1) == “,”] <- str_replace(string = Data$variable1[str_sub(string = Data$variable1, start = 1, end = 1) == “,”], pattern = “^\\,”, replacement = “”)
 Data$variable1[str_sub(string = Data$variable1, start = nchar(Data$variable1), end = nchar(Data$variable1)) == “,”] <- str_replace(string = Data$variable1[str_sub(string = Data$variable1, start = nchar(Data$variable1), end = nchar(Data$variable1)) == “,”], pattern = “\\,$”, replacement = “”)
