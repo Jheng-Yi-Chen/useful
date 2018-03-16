4940ca06d21f7a036908a23801ef2cb3c5d92592
Jheng-Yi Chen <deanst28@gmail.com>
2018-03-16 16:06:12 +0800

    Create charts.PerformanceSummary.R

---
+++ charts.PerformanceSummary.R
@@ -0,0 +1,15 @@
+library(quantmod); library(PerformanceAnalytics)
+library(magrittr)
+list <- c("^GSPC", "^DJI", "^TWII")
+getSymbols(list)
+duration = "2011/"
+
+close <- Cl(TWII[duration]) %>% na.omit()
+mv5 <- SMA(close, 5)
+sig <- ifelse(close < mv5, 1, 0)
+sig <- Lag(sig)
+roc <- ROC(type = "discrete", close)
+ret <- roc*sig
+charts.PerformanceSummary(ret)
+
+plot(TWII$TWII.Close)
