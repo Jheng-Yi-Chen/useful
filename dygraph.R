77a4c9c60df6ace652585935ad6104f016435b29
Jheng-Yi Chen <deanst28@gmail.com>
2018-05-03 13:29:24 +0800

    Update dygraph.R

--- dygraph.R
+++ dygraph.R
@@ -1,4 +1,5 @@
 Oil["2015/"] %>%
   dygraph() %>%
   dyRangeSelector() %>%
-  dyHighlight(highlightSeriesOpts = list(strokeWidth = 3))
+  dyHighlight(highlightSeriesOpts = list(strokeWidth = 3)) %>%
+  dyEvent("2015-12-01", "December 2015", labelLoc = "bottom", col = "gold")
